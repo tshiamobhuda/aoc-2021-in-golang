e0fcf92b19266ca9e67978abf39bde7f5a7a1143
Tshiamo Bhuda <tshiamobhuda@gmail.com>
2021-12-18 21:24:04 +0200

    Added basic project setup - go.mod - aoc main func

---
+++ aoc/main.go
@@ -0,0 +1,8 @@
+package main
+
+import "fmt"
+
+func main() {
+	// run the cli
+	fmt.Printf("--\tAdvent of Code 2021\t---\n")
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/tshiamobhuda/aoc-2021-in-go
+
+go 1.17
